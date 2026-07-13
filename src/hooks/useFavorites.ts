02689c79d5f2ceed84da661efa79ebb3044ae0c7
YAHOR RUDZIANKOU <rudenkov.egor04@mail.ru>
2026-07-13 11:39:47 +0300

    refactor(favorites): remove unnecessary useCallback from hook

--- src/hooks/useFavorites.ts
+++ src/hooks/useFavorites.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { getMovieId } from "../utils/getMovieId";
 
 export type IMovie = {
@@ -14,20 +14,18 @@ export type IMovie = {
 export function useFavorites() {
   const [favoriteFilms, setFavoriteFilms] = useState<IMovie[]>([]);
 
-  const toggleFavorite = useCallback(
-    (value: IMovie) => {
-      let currentId = getMovieId(value);
-      let hasId = favoriteFilms.some((item) => getMovieId(item) === currentId);
-      if (hasId) {
-        let noRepeatFilms = favoriteFilms.filter(
-          (item) => getMovieId(item) !== currentId,
-        );
-        setFavoriteFilms(noRepeatFilms);
-      } else {
-        setFavoriteFilms([...favoriteFilms, value]);
-      }
-    },
-    [favoriteFilms],
-  );
+  const toggleFavorite = (value: IMovie) => {
+    let currentId = getMovieId(value);
+    let hasId = favoriteFilms.some((item) => getMovieId(item) === currentId);
+    if (hasId) {
+      let noRepeatFilms = favoriteFilms.filter(
+        (item) => getMovieId(item) !== currentId,
+      );
+      setFavoriteFilms(noRepeatFilms);
+    } else {
+      setFavoriteFilms([...favoriteFilms, value]);
+    }
+  };
+
   return { favoriteFilms, toggleFavorite };
 }
